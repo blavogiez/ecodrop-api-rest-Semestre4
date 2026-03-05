810ecdb395c7c49633a538a0148d7c9cd7d875c0
Paul-Arnaud Delavitoire <paul-arnaud.delavictoire.etu@univ-lille.fr>
2026-03-05 14:50:35 +0100

    CollectionPointRestAPI WIP

---
+++ tomcat/webapps/ecodrop/WEB-INF/src/controller/CollectionPointRestAPI.java
@@ -0,0 +1,73 @@
+package controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dao.CollectionPointDAO;
+import model.dao.CollectionPointDAOPostgres;
+import model.dto.CollectionPoint;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+@WebServlet("/waste-type/*")
+public class CollectionPointRestAPI {
+
+    CollectionPointDAO dao = new CollectionPointDAOPostgres();
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String info = req.getPathInfo();
+
+        System.out.println(info);
+
+        if (info == null || info.equals("/")) {
+            Collection<CollectionPoint> l = dao.findAll();
+            String jsonstring = objectMapper.writeValueAsString(l);
+            out.print(jsonstring);
+            return;
+        }
+        String[] splits = info.split("/");
+        if (splits.length != 2) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        String id = splits[1];
+        CollectionPoint e = dao.findById(Integer.parseInt(id));
+        if (e == null) {
+            res.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        out.print(objectMapper.writeValueAsString(e));
+        return;
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        String data = new BufferedReader(new InputStreamReader(req.getInputStream())).readLine();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            CollectionPoint collectionPoint = objectMapper.readValue(data, CollectionPoint.class);
+            System.out.println(collectionPoint);
+
+            if (!dao.add(collectionPoint)) {
+                res.sendError(HttpServletResponse.SC_CONFLICT);
+                return;
+            }
+            PrintWriter out = res.getWriter();
+            String jsonstring = objectMapper.writeValueAsString(collectionPoint);
+            out.print(jsonstring);
+        } catch (Exception e) {
+            System.out.println("Could not save collection point : " + e.getMessage());
+        }
+        return;
+    }
+}
