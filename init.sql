43f3f4682d3f0ef736200b06eb5b7f2abc0bc4ef
Paul-Arnaud Delavitoire <paul-arnaud.delavictoire.etu@univ-lille.fr>
2026-03-05 13:58:55 +0100

    init.sql for merge

--- init.sql
+++ init.sql
@@ -1,5 +1,12 @@
 --- Création de la base avec idempotence
+DROP TABLE IF EXISTS Accepts CASCADE;
+DROP TABLE IF EXISTS WasteType CASCADE;
+DROP TABLE IF EXISTS CollectionPoint CASCADE;
 
 create table WasteType (
     id serial primary key 
+);
+
+CREATE TABLE CollectionPoint (
+
 )
