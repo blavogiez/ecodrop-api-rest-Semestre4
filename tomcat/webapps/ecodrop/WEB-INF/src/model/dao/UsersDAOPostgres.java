26a25d522e7b636f5a41be5733e81f82fd8ce526
Paul-Arnaud Delavitoire <paul-arnaud.delavictoire.etu@univ-lille.fr>
2026-03-12 14:54:39 +0100

    Finished UsersDAO + fixed bruno files addresses

--- tomcat/webapps/ecodrop/WEB-INF/src/model/dao/UsersDAOPostgres.java
+++ tomcat/webapps/ecodrop/WEB-INF/src/model/dao/UsersDAOPostgres.java
@@ -1,5 +1,85 @@
 package model.dao;
 
-class UsersDAOPostgres implements UsersDAOPostgres {
+import model.Role;
+import model.dto.Users;
+import utils.DS;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersDAOPostgres implements UsersDAO {
+
+    private static final DS DS = new DS();
+
+    @Override
+    public List<Users> findAll() {
+        List<Users> users = new ArrayList<>();
+
+        try (Connection con = DS.getConnection()) {
+            String query = "select * from Users";
+            PreparedStatement ps = con.prepareStatement(query);
+            System.out.println(ps);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String login = rs.getString("login");
+                String password = rs.getString("password");
+                Role role = Role.valueOf(rs.getString("role"));
+
+                users.add(new Users(id, login, password, role));
+            }
+        } catch (Exception e) {
+            System.err.println("Could not retrieve all Users " + " : " + e.getMessage());
+        }
+
+        return users;
+    }
+
+    @Override
+    public Users findById(int id) {
+        Users users = null;
+
+        try (Connection con = DS.getConnection()) {
+            String query = "select * from Users where id=?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            System.out.println(ps);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String login = rs.getString("login");
+                String password = rs.getString("password");
+                Role role = Role.valueOf(rs.getString("role"));
+
+                users = new Users(id, login, password, role);
+            }
+        } catch (Exception e) {
+            System.err.println("Could not find User with id " + id + " : " + e.getMessage());
+        }
+
+        return users;
+    }
+
+    @Override
+    public Users update(Users updated) {
+        try (Connection con = DS.getConnection()) {
+            String query = "update users SET login = ?, password = ?, role = ? where id = ?";
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, updated.getLogin());
+            ps.setString(2, updated.getPassword());
+            ps.setString(3, updated.getRole().toString());
+
+            System.out.println(ps);
+            ps.executeUpdate();
+            return updated;
+        } catch (Exception e) {
+            System.err.println("Could not update User with informations " + updated + " : " + e.getMessage());
+        }
+        return null;
+    }
 }
