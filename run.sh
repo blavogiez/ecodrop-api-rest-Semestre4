6980024b84ab019c5c77fe4e9064b4cb3542e3a3
Baptiste Lavogiez <baptiste.lavogiez.etu@univ-lille.fr>
2026-03-27 11:55:48 +0100

    feat(bruno): reset token utils

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # sudo docker compose down -v && sudo docker compose up -d --build 
-podman-compose down -v && podman-compose up -d --build 
 
-if [ $1 == "--test" ] 2>/dev/null
+if [ $1 == "build" ] 2>/dev/null
+then 
+    podman-compose down -v && podman-compose up -d --build 
+    if [ $2 == "and-test" ] 2>/dev/null
+    then 
+        podman exec -it -w /bruno-clean ecodrop_ecodrop_1 bru run --env local
+    fi
+fi
+
+if [ $1 == "only-test" ] 2>/dev/null
 then 
     podman exec -it -w /bruno-clean ecodrop_ecodrop_1 bru run --env local
 fi
